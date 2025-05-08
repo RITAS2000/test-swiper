6355a325d0bf59ed95fc5b12203ee25c7b95f319
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-05-09 02:09:15 +0300

    swiper

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper';
+import { Navigation, Keyboard, EffectCoverflow } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-coverflow';
+
+Swiper.use([Navigation, Keyboard, EffectCoverflow]);
+
+const swiper = new Swiper('.container-swiper', {
+  loop: true,
+  slidesPerView: 5,
+  centeredSlides: true,
+  initialSlide: 5,
+  spaceBetween: 100,
+  speed: 500,
+  loopAdditionalSlides: 0,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  navigation: {
+    nextEl: '[data-arrow="next"]',
+    prevEl: '[data-arrow="prev"]',
+  },
+  a11y: true,
+});
